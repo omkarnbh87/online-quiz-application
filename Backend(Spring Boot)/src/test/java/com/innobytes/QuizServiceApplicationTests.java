3fca1c5330d650315526130cd46f62a9036a00f5
Omkarnbh87 <contact.omkar87@gmail.com>
2024-06-20 21:52:37 +0530

    Initial setup done

---
+++ Backend(Spring Boot)/src/test/java/com/innobytes/QuizServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innobytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
